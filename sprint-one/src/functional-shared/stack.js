8434a7feefe6e285e0037757ee978dbc27b3bcea
Leighton <leighton.kaina@gmail.com>
2013-09-25 16:26:02 -0700

    passed all specs.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,26 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.count = 0; // Hint: set an initial value here
+  _.extend(instance, stackMethods);
+  // Implement the methods below
+
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+stackMethods.pop = function(){
+  this.count && this.count--;
+  var popVal = this.storage[this.count];
+  return popVal;
+};
+stackMethods.size = function(){
+  return this.count;
+};
