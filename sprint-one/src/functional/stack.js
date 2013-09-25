5f2297aef25449b62ea34cdd177ade800d9ff43a
Leighton <leighton.kaina@gmail.com>
2013-09-25 13:13:06 -0700

    stack working.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.pop = function(){
+    size && size--;
+    var popVal = storage[size];
+    delete storage[size];
+    return popVal;
+
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
