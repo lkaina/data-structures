8e648bde7c87cf9faf602a6ec0332f76bbe0f643
Leighton <leighton.kaina@gmail.com>
2013-09-25 17:40:30 -0700

    passed all specs.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,23 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+
+  // Use an object with numeric keys to store values
+  this.storage = {};
+  this.count = 0;
+  this.deqCount = 0;
+  // Hint: set an initial value here
+  
+  // Implement the methods below
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.count + this.deqCount] = value;
+  this.count++;
+};
+Queue.prototype.dequeue = function(){
+  this.count && this.count--;
+  this.deqCount++;
+  return this.storage[this.deqCount - 1];
+};
+Queue.prototype.size = function(){
+  return this.count;
+};
