52487efcc8aa1c959c6142d872e64a9653c05309
Leighton <leighton.kaina@gmail.com>
2013-09-25 17:28:11 -0700

    passed all specs.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,6 +1,27 @@
 var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.count = 0;
+  instance.deqCount = 0;
+  // Hint: set an initial value here
+  // Implement the methods below
+
+  return instance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  this.storage[this.count + this.deqCount] = value;
+  this.count++;
+};
+queueMethods.dequeue = function(){
+  this.count && this.count--;
+  this.deqCount++;
+  return this.storage[this.deqCount - 1];
+};
+queueMethods.size = function(){
+  return this.count;
+};
