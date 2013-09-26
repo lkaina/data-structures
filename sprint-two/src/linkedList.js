d2d88940eafec6db29784df0ee313045db496eff
Leighton <leighton.kaina@gmail.com>
2013-09-25 20:55:32 -0700

    finished addToTail and removeHead.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,13 +1,22 @@
-// Note: don't use an array to do this.
+ // Note: don't use an array to do this.
 var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if (!list.head) {
+      list.head = newNode;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    if (list.head) {
+      list.head.next = list.head;
+    }
   };
 
   list.contains = function(){
