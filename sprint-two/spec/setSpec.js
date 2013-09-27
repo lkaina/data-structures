8d76ddfacb3147804704670a8368e489eb35e9dd
Leighton <leighton.kaina@gmail.com>
2013-09-27 11:32:49 -0700

    create makeSet, pass specs

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -2,12 +2,37 @@ describe("set", function() {
   var set;
 
   beforeEach(function() {
-    set = makeSet();
+    set1 = makeSet();
+    set2 = makeSet();
   });
 
   it("should have methods named 'add', 'contains', and 'remove'", function() {
-    expect(set.add).toEqual(jasmine.any(Function));
-    expect(set.contains).toEqual(jasmine.any(Function));
-    expect(set.remove).toEqual(jasmine.any(Function));
+    expect(set1.add).toEqual(jasmine.any(Function));
+    expect(set1.contains).toEqual(jasmine.any(Function));
+    expect(set1.remove).toEqual(jasmine.any(Function));
+  });
+
+  it("should add a value to a set", function(){
+    set1.add("hello");
+    expect(set1._storage[0]).toEqual("hello");
+  });
+
+  it("should remove a value from a set", function(){
+    set1.add("hello");
+    set1.add("dog");
+    set1.remove("hello");
+    expect(set1._storage[0]).toEqual("dog");
+  });
+
+  it("should not duplicate values", function(){
+    set1.add("hello");
+    set1.add("hello");
+    set1.add("dog");
+    expect(set1._storage[1]).toEqual("dog");
+  });
+  it("should tell us if a value exists in the set", function(){
+    set1.add("hello");
+    set1.add("dog");
+    expect(set1.contains("dog")).toEqual(true);
   });
 });
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,32 @@
 var makeSet = function(){
   var set = Object.create(setPrototype); // fix me
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(){
+setPrototype.add = function(value){
+  if (!this.contains(value)){
+    this._storage.push(value);
+  }
 };
 
-setPrototype.contains = function(){
+setPrototype.contains = function(value){
+  for (var i = 0; i < this._storage.length; i++){
+    if (this._storage[i] === value){
+      return true;
+    }
+  }
+  return false;
 };
 
-setPrototype.remove = function(){
+setPrototype.remove = function(value){
+  if (this.contains(value)){
+    for (var i = 0; i < this._storage.length; i++){
+      if (this._storage[i] === value){
+        this._storage.splice(i, 1);
+      }
+    }
+  }
 };
