e6fa223c6c214592599188a4b846c8657c0d8607
Leighton <leighton.kaina@gmail.com>
2013-09-26 20:54:01 -0700

    passed all specs.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,15 +1,41 @@
 var makeTree = function(){
   var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
 
+  newTree.value = undefined;
+  newTree.children = [];
+  extend(treeMethods, newTree);
   return newTree;
+};
+
+var extend = function(from, to){
+  for (var key in from){
+    to[key]=from[key];
+  }
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
+  var child = makeTree();
+  child.value = value;
+  this.children.push(child);
 };
 
-treeMethods.contains = function(){
+treeMethods.contains = function(value){
+  //var truth = 0;
+  if (this.value === value) {
+    return true;
+  }
+  else if (this.children.length) {
+    for (var i = 0; i < this.children.length; i++){
+      if (i === this.children.length - 1) {
+        return this.children[i].contains(value);
+      }
+      else {
+        this.children[i].contains(value);
+      }
+    }
+  }
+  return false;
 };
+
