2cee1f31df0250a839b8cb6bd3a8265ac41633f7
Leighton <leighton.kaina@gmail.com>
2013-09-27 12:38:04 -0700

    write .insert, start .retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -15,11 +15,25 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   console.log(i);
-  this._storage.set(i, v);
+  //given i, we can find storage[index], aka bucket, if array bucket doesn't exist
+  // pass empty bucket
+  var bucket;
+  if (this._storage.get(i)) {
+    bucket = this._storage.get(i);
+  } else {
+    bucket = [];
+  }
+  bucket.push([k,v]);
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i].each(function(value, index, storage){
+    if (value[0] === k){
+      return value[1];
+    }
+  });
   return this._storage.get(i);
 };
 
